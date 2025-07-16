18f871f97fb99d63f8f2750ee690b6c58fdc27d5
Alejandro Lynch <alynch@hopscotch.ca>
2025-07-16 10:56:19 -0400

    simplify App.svelte component

--- src/App.svelte
+++ src/App.svelte
@@ -6,26 +6,13 @@
 
 <main>
   <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
   <h1>Vite + Svelte</h1>
 
   <div class="card">
-    <Counter />
+    <Counter count={99}/>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
